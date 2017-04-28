9cc2da090a02a94304fb39e743985f0230de03df
Bas Elbers <baselbers@hotmail.com>
2017-04-28 12:10:17 +0200

    Changed 'sun-exta' to 'simsun' to correctly substitute CJK chars.

--- config_lang2fonts.php
+++ config_lang2fonts.php
@@ -260,7 +260,7 @@ function GetLangOpts($llcc, $adobeCJK, &$fontdata)
 		//CASE "ccp":	// CHAKMA
 		//CASE "lep":	// LEPCHA
 		CASE "lif":  // LIMBU
-			$unifont = "sun-exta";
+			$unifont = "simsun";
 			break;
 		//CASE "sat":	// OL_CHIKI
 		//CASE "saz":	// SAURASHTRA
@@ -338,19 +338,19 @@ function GetLangOpts($llcc, $adobeCJK, &$fontdata)
 				if ($adobeCJK) {
 					$unifont = "big5";
 				} else {
-					$unifont = "sun-exta";
+					$unifont = "simsun";
 				}
 			} else if ($country == "CN") {
 				if ($adobeCJK) {
 					$unifont = "gb";
 				} else {
-					$unifont = "sun-exta";
+					$unifont = "simsun";
 				}
 			} else {
 				if ($adobeCJK) {
 					$unifont = "gb";
 				} else {
-					$unifont = "sun-exta";
+					$unifont = "simsun";
 				}
 			}
 			break;
@@ -365,14 +365,14 @@ function GetLangOpts($llcc, $adobeCJK, &$fontdata)
 			if ($adobeCJK) {
 				$unifont = "sjis";
 			} else {
-				$unifont = "sun-exta";
+				$unifont = "simsun";
 			}
 			break;
 		CASE "ii": CASE "iii": // Nuosu; Yi
 			if ($adobeCJK) {
 				$unifont = "gb";
 			} else {
-				$unifont = "sun-exta";
+				$unifont = "simsun";
 			}
 		CASE "lis":  // LISU
 			$unifont = "quivira";
@@ -417,7 +417,7 @@ function GetLangOpts($llcc, $adobeCJK, &$fontdata)
 					$unifont = "aegean";
 					break;
 				CASE "runr": // RUNIC
-					$unifont = "sun-exta";
+					$unifont = "simsun";
 					break;
 				CASE "shaw": // SHAVIAN
 					$unifont = "mph2bdamase";
@@ -484,15 +484,15 @@ function GetLangOpts($llcc, $adobeCJK, &$fontdata)
 					if ($adobeCJK) {
 						$unifont = "gb";
 					} else {
-						$unifont = "sun-exta";
+						$unifont = "simsun";
 					}
 					break;
 				CASE "bopo": // BOPOMOFO
-					$unifont = "sun-exta";
+					$unifont = "simsun";
 					break;
 				//CASE "plrd":	// MIAO
 				CASE "yiii": // YI
-					$unifont = "sun-exta";
+					$unifont = "simsun";
 					break;
 
 				/* American */
